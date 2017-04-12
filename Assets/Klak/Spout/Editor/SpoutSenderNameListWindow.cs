481790154a2d844cdc46d9781c76a436cb974a4e
Keijiro Takahashi <keijiro@gmail.com>
2017-04-12 23:38:22 +0900

    Added sender name list.

---
+++ Assets/Klak/Spout/Editor/SpoutSenderNameListWindow.cs
@@ -0,0 +1,38 @@
+// KlakSpout - Spout realtime video sharing plugin for Unity
+// https://github.com/keijiro/KlakSpout
+using UnityEngine;
+using UnityEditor;
+
+namespace Klak.Spout
+{
+    // Spout sender list window
+    public class SenderListWindow : EditorWindow
+    {
+        [MenuItem("Window/Klak/Spout Sender List")]
+        static void Init()
+        {
+            EditorWindow.GetWindow<SenderListWindow>("Spout Senders").Show();
+        }
+
+        void OnGUI()
+        {
+            var count = PluginEntry.CountSharedTextures();
+
+            EditorGUILayout.Space();
+            EditorGUI.indentLevel++;
+
+            if (count == 0)
+                EditorGUILayout.LabelField("No sender detected.");
+            else
+                EditorGUILayout.LabelField(count + " sender(s) detected.");
+
+            for (var i = 0; i < count; i++)
+            {
+                var name = PluginEntry.GetSharedTextureNameString(i);
+                if (name != null) EditorGUILayout.SelectableLabel(name);
+            }
+
+            EditorGUI.indentLevel--;
+        }
+    }
+}
