d209e37dc340cdd779dff77519deba98df854862
Keijiro Takahashi <keijiro@gmail.com>
2018-09-12 15:52:46 +0900

    Set implicit default value

--- Assets/Klak/Spout/Runtime/SpoutReceiver.cs
+++ Assets/Klak/Spout/Runtime/SpoutReceiver.cs
@@ -40,7 +40,7 @@ namespace Klak.Spout
             set { _targetRenderer = value; }
         }
 
-        [SerializeField] string _targetMaterialProperty;
+        [SerializeField] string _targetMaterialProperty = null;
 
         public string targetMaterialProperty {
             get { return _targetMaterialProperty; }
