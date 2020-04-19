6ad8be32eeb6c3b5a7a751bb459c1cceee05bdb6
Keijiro Takahashi <keijiro@gmail.com>
2020-04-19 17:33:21 +0900

    Change to use WSL mingw environment for plugin compilation

---
+++ Plugin/build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+GXX="x86_64-w64-mingw32-g++-posix"
+
+compile()
+{
+    SRC_FILE="$1"
+    OBJ_FILE="build/$(basename -s .cpp $SRC_FILE).o"
+    $GXX -c -Wall -O2 -I. -IKlakSpout $SRC_FILE -o $OBJ_FILE
+}
+
+compile KlakSpout/KlakSpout.cpp
+compile Spout/SpoutDirectX.cpp
+compile Spout/SpoutSenderNames.cpp
+compile Spout/SpoutSharedMemory.cpp
+
+$GXX -shared -o build/KlakSpout.dll build/*.o -s \
+     -Wl,--subsystem,windows -static -ldxgi -ld3d9 -ld3d11
